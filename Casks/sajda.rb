3a08ab678f8ff4d1b44fc29daca5985f3393516b
abrar <abrar.zharif@gmail.com>
2026-09-23 19:36:23 +0700

    sajda 3.4.0

--- Casks/sajda.rb
+++ Casks/sajda.rb
@@ -1,6 +1,6 @@
 cask "sajda" do
-  version "3.2.0"
-  sha256 "79f08211503f89a5e0baabd149071058317effd5b53126b623ea0d8088c4aeca"
+  version "3.4.0"
+  sha256 "8457fa4ab04800a11f7bf27ae85c401e49afcbb1b7bf08fa991000219895a1d6"
 
   url "https://github.com/ikoshura/Sajda/releases/download/v#{version}/Sajda-#{version}.dmg"
   name "Sajda"
