4e1aad3927e5336b610853952ed186db58760da5
abrar <abrar.zharif@gmail.com>
2026-09-26 06:06:52 +0700

    chore: bump sajda cask to 4.0.1 (requires macOS Sonoma)

--- Casks/sajda.rb
+++ Casks/sajda.rb
@@ -1,13 +1,13 @@
 cask "sajda" do
-  version "3.6.4"
-  sha256 "754572616518020bdd369c62302704cedcb7fb570e06b5a8d71bbf3b80aaed52"
+  version "4.0.1"
+  sha256 "4e9dc0ff68e710862213352a2b1a7e3ab880e3e601a6e80c17618246d5a7ca5e"
 
   url "https://github.com/ikoshura/Sajda/releases/download/v#{version}/Sajda-#{version}.dmg"
   name "Sajda"
   desc "Minimalist prayer times menu bar app"
   homepage "https://github.com/ikoshura/Sajda"
 
-  depends_on macos: :ventura
+  depends_on macos: :sonoma
 
   app "Sajda.app"
 
