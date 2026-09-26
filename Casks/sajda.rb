5bb1bb7a20891e3149bc45ec143725fcec63acde
abrar <abrar.zharif@gmail.com>
2026-09-26 20:58:07 +0700

    chore: bump sajda cask to 4.1.0

--- Casks/sajda.rb
+++ Casks/sajda.rb
@@ -1,6 +1,6 @@
 cask "sajda" do
-  version "4.0.3"
-  sha256 "4a2ac5d4c6276c4116cc408bbe53bf94d463dd6993b36573667759a4c690847c"
+  version "4.1.0"
+  sha256 "1f80b0f43b3cc121d0fcf3350533ebc699439c2ef913b85eff956e24ea08ecde"
 
   url "https://github.com/ikoshura/Sajda/releases/download/v#{version}/Sajda-#{version}.dmg"
   name "Sajda"
